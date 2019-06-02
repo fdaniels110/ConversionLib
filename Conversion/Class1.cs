bc5125a6228cc106059a3c2d43a5c910cec69a17
fdaniels110 <frankdaniels110@gmail.com>
2019-06-02 16:16:53 -0700

    Add project files.

---
+++ Conversion/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConversionTest
+{
+    public class MainConverter
+    {
+
+    }
+
+    public interface IConversion
+    {
+        string Name { get; }
+        string Version { get; }
+        object Input { get; }
+        object Output { get; }
+    }
+}
